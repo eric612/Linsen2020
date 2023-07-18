b5f16d6718545a1db27a8bb6a71054bbf4171369
Eric Liu <ericliu612@gmail.com>
2023-07-18 19:38:46 +0800

    Update attack_timer.js

--- attack_timer.js
+++ attack_timer.js
@@ -46,8 +46,8 @@ function last_counter() {
 
 // 3. Add event handler
 button.addEventListener ("click", function() {
-  let dtime = new Date();
-  dtime.setMinutes(dtime.getMinutes()+parseInt(path_time_input.value));
+  let dtime = new Date(time_input);
+  //dtime.setMinutes(dtime.getMinutes()+parseInt(path_time_input.value));
   if (dtime  - new Date() > 1500) {
     setTimeout(last_counter, dtime - new Date() - 1000);
   } else {
